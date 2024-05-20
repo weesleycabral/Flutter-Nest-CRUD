011861171ef702fe76f9941eed274d544d176fc6
weesleycabral <weesleylima@hotmail.com>
2024-05-20 09:50:16 -0300

    chore: Add Categories module and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThingsModule } from './things/things.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ThingsModule],
+  imports: [ThingsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
